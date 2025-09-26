cfa77b4d820cf420f733c76a4ddb07928b44d51c
Hariom Tiwari <17hariom09@gmail.com>
2025-09-27 02:26:22 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "import.meta.env.VITE_API_BASE_URL",
+      "/api": import.meta.env.VITE_API_BASE_URL,
     },
   },
   plugins: [react()],
