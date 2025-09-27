89e35ad1f2b3cab7b09e91c3c35f391611f27d09
Hariom Tiwari <17hariom09@gmail.com>
2025-09-27 21:02:40 +0530

    updated cors to accept req from backend url

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import rateLimit from 'express-rate-limit';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import db from './config/database.js';
-import authRoutes from './routes/authRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
-import usersRoutes from './routes/usersRoutes.js';
-import productRoutes from './routes/productRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import db from "./config/database.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import usersRoutes from "./routes/usersRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 // import paymentRoutes from './routes/paymentRoutes.js'
 
 dotenv.config();
@@ -29,28 +29,33 @@ app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://med-sale-frontend.vercel.app"],
+    credentials: true,
+  })
+);
 
 // Set up static file serving
-app.use(express.static(new URL('./public', import.meta.url).pathname));
-app.use(express.static(new URL('./public/uploads', import.meta.url).pathname));
+app.use(express.static(new URL("./public", import.meta.url).pathname));
+app.use(express.static(new URL("./public/uploads", import.meta.url).pathname));
 
 // Set up routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/admin', adminRoutes);
-app.use('/api/v1/users', usersRoutes);
-app.use('/api/v1/products', productRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/users", usersRoutes);
+app.use("/api/v1/products", productRoutes);
 // app.use('/api/v1/payment', paymentRoutes);
 
 // Database Connection
-db.on('open', () => {
-  console.log('Database connected');
+db.on("open", () => {
+  console.log("Database connected");
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 // Port connection
@@ -60,8 +65,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-
 
 // // Enable additional helmet features
 // app.use(
