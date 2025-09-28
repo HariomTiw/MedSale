ad9b0b34c4c52be00c9bc475247790c3f26785e2
Hariom Tiwari <17hariom09@gmail.com>
2025-09-29 01:52:02 +0530

    refactor: update API base URLs to use environment variable

--- frontend/src/api/AuthApi.jsx
+++ frontend/src/api/AuthApi.jsx
@@ -1,7 +1,7 @@
 // api/authApi.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api/v1/auth';
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/auth`;
 
 const authApi = {
   registerUser: async (userData) => {
